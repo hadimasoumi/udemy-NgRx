f4a507a46c8768cce158ff8b2d6da360236fe687
Hadi Masoumi <st.sbu@yahoo.com>
2023-03-24 23:04:04 +0300

    ng add @ngrx/store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthModule } from './auth/auth.module';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { StoreModule } from '@ngrx/store';
 
 const routes: Routes = [
   {
@@ -40,6 +41,7 @@ const routes: Routes = [
     MatListModule,
     MatToolbarModule,
     AuthModule.forRoot(),
+    StoreModule.forRoot({}, {}),
   ],
   bootstrap: [AppComponent],
 })
