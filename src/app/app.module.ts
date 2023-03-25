b4ffebf730c383c9aa8094b0a3c4fb54b1a0b824
Hadi Masoumi <st.sbu@yahoo.com>
2023-03-25 15:40:57 +0300

    23. Runtime checks: strict checking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,7 +48,14 @@ const routes: Routes = [
     MatListModule,
     MatToolbarModule,
     AuthModule.forRoot(),
-    StoreModule.forRoot(reducers),
+    StoreModule.forRoot(reducers, {
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+        strictStateSerializability: true,
+        strictActionSerializability: true,
+      },
+    }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
     EffectsModule.forRoot(),
     StoreRouterConnectingModule.forRoot({
