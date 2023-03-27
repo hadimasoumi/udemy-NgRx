203f4ce38fc96fff1e0ea153420897696e196ee5
Hadi Masoumi <st.sbu@yahoo.com>
2023-03-27 17:33:13 +0300

    41. NgRx Loaded flag

--- src/app/courses/services/courses.resolver.ts
+++ src/app/courses/services/courses.resolver.ts
@@ -2,37 +2,21 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { filter, finalize, first, map, tap } from 'rxjs/operators';
-import { select, Store } from '@ngrx/store';
-import { CoursesActions } from '../courses.ngrx.store/courses.actions';
-import { CourseState } from '../courses.ngrx.store/courses.reducers';
-import { coursesSelectors } from '../courses.ngrx.store/courses.selectors';
 import { CourseEntityService } from './course-entity.service';
 
 @Injectable()
 export class CoursesResolver implements Resolve<boolean> {
-  // loading = false;
-  // constructor(private store: Store<CourseState>) {}
-
-  // resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
-  //   return this.store.pipe(
-  //     select(coursesSelectors.areCoursesLoaded),
-  //     tap(coursesLoaded => {
-  //       if (!this.loading && !coursesLoaded) {
-  //         this.loading = true;
-  //         this.store.dispatch(CoursesActions.loadAllCourses());
-  //       }
-  //     }),
-  //     filter(coursesLoaded => coursesLoaded),
-  //     first(),
-  //     finalize(() => {
-  //       this.loading = false;
-  //     })
-  //   );
-  // }
-
   constructor(private courseService: CourseEntityService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
-    return this.courseService.getAll().pipe(map(courses => !!courses));
+    return this.courseService.loaded$.pipe(
+      tap(loaded => {
+        if (!loaded) {
+          this.courseService.getAll().pipe(map(courses => !!courses));
+        }
+      }),
+      filter(loaded => !!loaded),
+      first()
+    );
   }
 }
